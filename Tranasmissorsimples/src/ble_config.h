4a5243fe0264fce943dfe206b813c77463a97313
Rafael <rafael.mattos.gama@gmail.com>
2025-12-05 17:01:06 +0000

    Somente app

--- Tranasmissorsimples/src/ble_config.h
+++
@@ -1,8 +0,0 @@
-#ifndef BLE_CONFIG_H
-#define BLE_CONFIG_H
-
-void startBLEConfigMode();
-void stopBLE();
-void runBLEConfigLoop();
-
-#endif // BLE_CONFIG_H
--- Tranasmissorsimples/src/espnow.h
+++
@@ -1,9 +0,0 @@
-#ifndef ESPNOW_H
-#define ESPNOW_H
-
-#include "sensors.h"
-
-bool startEspNow();
-void sendEspNow(const DadosEnvio &pacote);
-
-#endif // ESPNOW_H
